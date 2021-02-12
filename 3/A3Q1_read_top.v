b0f3a0e54e2b53053e631cb65179687974d2c724
antreev-brar <antreev.brar@gmail.com>
2021-02-12 21:42:40 +0530

    finalQ1

--- 3/A3Q1_read_top.v
+++ 3/A3Q1_read_top.v
@@ -6,16 +6,22 @@ module top;
    reg [3:0] row_num;
    wire output_valid;
    wire [31:0] out;
+
+   reg [5:0]count_cycle;
    
    read M(clk, row_num ,input_valid, output_valid,out);
 
     always @(output_valid)
     begin
         if(output_valid == 1'b1)begin
-         $display("< %d >  %d %b ",$time ,out , output_valid);
+         $display("< %d > row_num= %d content= %d  count_cycle=> %d",$time ,row_num , out, count_cycle);
         end
     end
 
+    always @(posedge clk)
+    begin
+        count_cycle <= count_cycle+1;
+    end
 
    initial begin
        #1000
@@ -32,33 +38,46 @@ module top;
     end
     end 
     initial begin
+        count_cycle=0;
         #3
         input_valid=1; row_num=0;
-        #4
+        #10
         input_valid=0;
-        #26
+        #20
         input_valid=1; row_num=0;
-        #4
+        #10
         input_valid=0;
-        #26
+        #20
         input_valid=1; row_num=0;
-         #4
+        #10
         input_valid=0;
-        #26
+        #20
         input_valid=1; row_num=1;
-         #4
+        #10
         input_valid=0;
-        #26
+        #20
         input_valid=1; row_num=1;
-         #4
+        #10
         input_valid=0;
-        #26
+        #20
         input_valid=1; row_num=2;
-         #4
+        #10
         input_valid=0;
-        #26
+        #20
         input_valid=1; row_num=3;
-         #4
+        #10
+        input_valid=0;
+        #20
+        input_valid=1; row_num=7;
+        #10
+        input_valid=0;
+        #20
+        input_valid=1; row_num=8;
+        #10
+        input_valid=0;
+        #20
+        input_valid=1; row_num=8;
+        #10
         input_valid=0;
     end 
 endmodule
