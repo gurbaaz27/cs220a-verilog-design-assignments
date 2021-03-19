f55ddc4f110e4bd05cf5e0dd1daa0f104390142a
antreev-brar <antreev.brar@gmail.com>
2021-03-20 01:07:26 +0530

    moosetape

--- 6/process.v
+++ 6/process.v
@@ -1,33 +1,84 @@
-module process(valid);
+module process(clk,valid,read_adr1 ,read_adr2, write_adr ,data,done  ,read1, read2  );
 
-    always @( posedge clk)
-    begin
-        
-        case (valid)
-            3'b000 : begin
-               
-            end
-            3'b001 : begin
-                
-            end
-            3'b010 : begin
-                
-            end
-            3'b011 : begin
-                
-            end
-            3'b100 : begin
-                
-            end
-            3'b101 : begin
-                
-            end
-            3'b110 : begin
-                
-            end
-            3'b111 : begin
-                
-            end
-        endcase
+    input [2:0] valid;
+    input [4:0] read_adr1;
+    input [4:0] read_adr2;
+    input [4:0] write_adr;
+    input [15:0] data;
+    input clk;
+    reg  read1_valid ;
+    reg  read2_valid ;
+    reg  write_valid ;
+    output [15:0] read1;
+    output [15:0] read2;
+    wire [15:0] read1;
+    wire [15:0] read2;
+    reg [5:0] counter=1;
+
+    wire [15:0] read1_from_reg;
+    wire [15:0] read2_from_reg;
+
+    output done;
+    reg done;
+
+    initial begin
+        read1_valid <=0;
+        read2_valid <=0;
+        write_valid <=0;
     end
+
+    registerfile M(clk,read_adr1 ,read_adr2, write_adr ,data , read1_valid, read2_valid , write_valid , read1_from_reg, read2_from_reg);
+  
+
+    always @(counter or valid )
+    begin
+        $display("time =%d counter = %d , valid = %b , ",$time,counter, valid);
+        if(counter == 0) begin
+           
+        
+            case (valid)
+                3'b000 : begin
+                    counter <= 2;
+                    read1_valid <=0; read2_valid <=0;  write_valid <= 1;
+                end
+                3'b001 : begin
+                    counter <= 2;
+                    read1_valid <=1; read2_valid <=0;  write_valid <= 0;
+                end
+                3'b010 : begin
+                    counter <= 2;
+                    read1_valid <=1; read2_valid <=1;  write_valid <= 0;
+                end
+                3'b011 : begin
+                    counter <= 2;
+                    read1_valid <=1; read2_valid <=0;  write_valid <= 1;
+                end
+                3'b100 : begin
+                    counter <= 2;
+                    read1_valid <=1; read2_valid <=1;  write_valid <= 1;
+                end
+                3'b101 : begin
+                
+                end
+                3'b110 : begin
+                
+                end
+                3'b111 : begin
+                
+                end
+            endcase
+        end
+        if(counter == 0) begin
+            done <= `TICK 1;
+        end
+    end
+
+    always @(posedge clk) begin
+        done = 0;
+        counter <= counter - 1;
+    end
+
+    assign  read1 = read1_from_reg;
+    assign  read2 = read2_from_reg;
+
 endmodule
--- 6/top.v
+++ 6/top.v
@@ -5,15 +5,18 @@ module top;
     reg [4:0] input_adr1;
     reg [4:0] input_adr2;
     reg [4:0] write_adr;
-    reg valid;
+    reg read1_valid;
+    reg read2_valid;
+    reg write_valid;
+    reg [2:0] valid;
     wire [15:0] read1;
     wire [15:0] read2;   
     reg [15:0] data;
 
 
-    registerfile M(clk,input_adr1 ,input_adr2, write_adr ,data , valid , read1, read2);
+    process P(clk,valid,input_adr1 ,input_adr2, write_adr ,data,done ,read1, read2  );
 
-    always @(posedge clk)
+    always @(posedge done)
     begin
          $display("time=%d: read_adr1 = %d, read1 = %d ------ read_adr2 => %d , read2 => %d",$time , input_adr1 , read1 ,  input_adr2 , read2);
     end
@@ -37,20 +40,20 @@ module top;
 
     initial begin
         #5
-        input_adr1 = 0 ;  input_adr2 = 1 ;  write_adr = 0 ; data = 1 ; valid =1;
-        #10
-        input_adr1 = 0 ;  input_adr2 = 1 ;  write_adr = 1 ; data = 2 ; valid =1;
-        #10
-        input_adr1 = 2 ;  input_adr2 = 1 ;  write_adr = 2 ; data = 3 ; valid =1;
-        #10
-        input_adr1 = 2 ;  input_adr2 = 3 ;  write_adr = 3 ; data = 4 ; valid =1;
-        #10
-        input_adr1 = 3 ;  input_adr2 = 4 ;  write_adr = 4 ; data = 5 ; valid =1;
-        #10
-        input_adr1 = 4 ;  input_adr2 = 5 ;  write_adr = 0 ; data = 6 ; valid =1;
-        #10
-        input_adr1 = 0 ;  input_adr2 = 1 ;  write_adr = 0 ; data = 0 ; valid =1;
-        #10
+        input_adr1 = 0 ;  input_adr2 = 1 ;  write_adr = 0 ; data = 1 ; valid = 3'b100;
+        #20
+        input_adr1 = 0 ;  input_adr2 = 1 ;  write_adr = 1 ; data = 2 ; valid = 3'b100;
+        #20
+        input_adr1 = 2 ;  input_adr2 = 1 ;  write_adr = 2 ; data = 3 ; valid = 3'b100;
+        #20
+        input_adr1 = 2 ;  input_adr2 = 3 ;  write_adr = 3 ; data = 4 ; valid = 3'b100;
+        #20
+        input_adr1 = 3 ;  input_adr2 = 4 ;  write_adr = 4 ; data = 5 ; valid = 3'b100;
+        #20
+        input_adr1 = 4 ;  input_adr2 = 5 ;  write_adr = 0 ; data = 6 ; valid = 3'b100;
+        #20
+        input_adr1 = 0 ;  input_adr2 = 1 ;  write_adr = 0 ; data = 0 ; valid = 3'b100;
+        #20
         $finish;
     end
     
