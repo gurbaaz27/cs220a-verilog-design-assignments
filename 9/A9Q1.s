ec90203b06cc5d465d39cc5c10a93881f65cf002
gurbaaz27 <gurbaazsn123@gmail.com>
2021-04-11 16:25:46 +0530

    A9Q1 added

--- 9/A9Q1.s
+++ 9/A9Q1.s
@@ -1,100 +1,104 @@
+## CS220 Assignment #9
+## Group #9
+## Question 1
+## Write a recursive function to compute the nth Fibonacci number. 
+## Write a main function that accepts an integer n as input (from keyboard) and 
+## prints the first n Fibonacci numbers on the display as a comma separated series.
 .text
-main:
-# Prompt user to input non-negative number
-la $a0,prompt   
-li $v0,4
-syscall
+main:   
+        la $a0, input       # User prompt for a non-negative integer input 
+        li $v0, 4
+        syscall
 
-li $v0,5    #Read the number(n)
-syscall
+        li $v0, 5           # Reads the integer n
+        syscall
 
-move $t2,$v0    # n to $t2
+        move $t2, $v0       # Move n to $t2
 
-move $t3, $0 #initialise starting variable to 1
-# Call function to get fibonnacci #n
-loop:
-addi $t3 , $t3 ,1
-move $a0,$t3
-move $v0,$t3
-jal fib     #call fib (n)
-move $t4,$v0    #result is in $t3
-
-# Output message and n
-#la $a0,result   #Print F_
-#li $v0,4
-#syscall
-
-#move $a0,$t3    #Print n
-#li $v0,1
-#syscall
+        move $t3, $0                                    
 
 
-move $a0,$t4    #Print the answer
-li $v0,1
-syscall
+loop:   
+        addi $t3, $t3, 1    # Initialise starting variable to 1
+        move $a0, $t3
+        move $v0, $t3
+        jal fib             # Call function to get fib(n)
+        move $t4, $v0       
 
-slt $t5 , $t3 , $t2
-beq $t5 , $0 , jump
-la $a0,result2  #Print =
-li $v0,4
-syscall
-jump:
+        move $a0, $t4       # Prints the value of fib (n)
+        li $v0, 1
+        syscall
 
-bne $t3 , $t2, loop
+        slt $t5, $t3, $t2
+        beq $t5, $0, jump
+        la $a0, sep         # Prints the separator ', '
+        li $v0,4
+        syscall
 
-la $a0,endl #Print '\n'
-li $v0,4
-syscall
 
-# End program
-li $v0,10
-syscall
+jump:   
+        bne $t3, $t2, loop
+
+        la $a0, endl        # Print '\n'
+        li $v0 ,4
+        syscall
+
+        # End program
+        li $v0, 10
+        syscall
+
 
 fib:
-# Compute and return fibonacci number
-beqz $a0,zero   #if n=0 return 0
-beq $a0,1,one   #if n=1 return 1
+        # Compute and return fibonacci number
+        beqz $a0, zero      # if n==0 return 0
+        beq $a0, 1, one     # if n==1 return 1
 
-#Calling fib(n-1)
-sub $sp,$sp,4   #storing return address on stack
-sw $ra,0($sp)
+        # Calling fib(n-1)------------------------------------
+        sub $sp, $sp, 4     # Store return address on stack
+        sw $ra, 0($sp)
 
-sub $a0,$a0,1   #n-1
-jal fib     #fib(n-1)
-add $a0,$a0,1
+        sub $a0, $a0, 1     # n-1
+        jal fib             # fib(n-1)
+        add $a0, $a0, 1
 
-lw $ra,0($sp)   #restoring return address from stack
-add $sp,$sp,4
+        lw $ra, 0($sp)      # Restore return address from stack
+        add $sp, $sp, 4
 
 
-sub $sp,$sp,4   #Push return value to stack
-sw $v0,0($sp)
-#Calling fib(n-2)
-sub $sp,$sp,4   #storing return address on stack
-sw $ra,0($sp)
+        sub $sp, $sp, 4     # Push return value to stack
+        sw $v0, 0($sp)
+        #------------------------------------------------------
 
-sub $a0,$a0,2   #n-2
-jal fib     #fib(n-2)
-add $a0,$a0,2
+        # Calling fib(n-2)-------------------------------------
+        sub $sp, $sp, 4     # storing return address on stack
+        sw $ra, 0($sp)
 
-lw $ra,0($sp)   #restoring return address from stack
-add $sp,$sp,4
-#---------------
-lw $s7,0($sp)   #Pop return value from stack
-add $sp,$sp,4
+        sub $a0, $a0,2      # n-2
+        jal fib             # fib(n-2)
+        add $a0, $a0,2
 
-add $v0,$v0,$s7 # f(n - 2)+fib(n-1)
-jr $ra # decrement/next in stack
+        lw $ra, 0($sp)      # Restore return address from stack
+        add $sp, $sp,4
 
-zero:
-li $v0,0
-jr $ra
-one:
-li $v0,1
-jr $ra
+        lw $s7, 0($sp)      # Pop return value from stack
+        add $sp, $sp, 4
+        #------------------------------------------------------
+
+        add $v0, $v0, $s7   # Compute fib(n) = fib(n-2) + fib(n-1)
+        jr $ra              # Decrement the stack
+
+
+zero:   
+        li $v0, 0
+        jr $ra  
+
+
+one:    
+        li $v0, 1
+        jr $ra
+
 
 .data
-prompt: .asciiz "This program calculates Fibonacci sequence with recursive functions.\nEnter a non-negative number: "
-result: .asciiz "F_"
-result2: .asciiz " , "
-endl: .asciiz "\n"
+input:      .asciiz "Assignment 9 submission by Group #9\nQ1. The following MIPS program computes nth Fibonacci number recursively and prints the first n Fibonacci numbers.\nEnter a non-negative integer n: "
+sep:        .asciiz ", "
+endl:       .asciiz "\n"
